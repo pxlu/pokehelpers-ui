bb74ced4690fb9494abf423a632df482fe1735bb
Peter Lu <peterlu94@gmail.com>
2022-11-18 00:22:09 -0500

    Added two link placeholders, added theme toggle
    Link placeholders were added for `About` and `Contact`, and added a toggle for day/night mode in the future

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,6 +1,10 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+
+// Component Imports
+import MaterialUISwitch from "./customSwitch";
 
 const drawerWidth = 240;
 
@@ -8,12 +12,34 @@ const Appbar = () => {
   return (
     <AppBar
       position="fixed"
-      sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      sx={{
+        width: `calc(100% - ${drawerWidth}px)`,
+        ml: `${drawerWidth}px`,
+        backgroundColor: "#000000",
+      }}
     >
       <Toolbar>
-        <Typography variant="h6" noWrap component="div">
-          Permanent drawer
-        </Typography>
+        <Grid container direction="row" spacing={1}>
+          <Grid item>
+            <Link
+              href="#"
+              color="#ffffff"
+              underline="hover"
+              sx={{
+                borderRight: "0.10em solid grey",
+                padding: "0.5em",
+              }}
+            >
+              About
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="#" color="#ffffff" underline="hover">
+              Contact
+            </Link>
+          </Grid>
+        </Grid>
+        <MaterialUISwitch sx={{ m: 1 }} defaultChecked />
       </Toolbar>
     </AppBar>
   );
